2bb552dde3a8f798df4e4c38ea1564ee7a3d64c4
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2020-11-04 18:09:17 +0530

    code cleanup

--- Sonic-Download-Manger/Startup.cs
+++ Sonic-Download-Manger/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Sonic_Download_Manger.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
